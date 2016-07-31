412e365228db0a1a74f7f4f90bce874dd88d47bf
Lucas Overland <loverland@cars.com>
2016-07-31 11:34:23 -0500

    generation of assets

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 //RUN THE TRAP
 'use strict'
+const     fs = require('fs')
 const server = require('./lib/server')
 
 
+
+
+generateAssets()
 server.listen(3000)
-console.log('Listing on port 3000')
+
+console.log('Server Started...')
+console.log('Listing on port 3000...')
+
+
+
+
+/**
+ * [generateAssets description]
+ * @return {[type]} [description]
+ */
+function generateAssets() {
+
+    console.log('Generating Assets...');
+
+    //create alloy editor javascript file
+    fs.createReadStream('node_modules/alloyeditor/dist/alloy-editor/alloy-editor-all-min.js')
+        .pipe(fs.createWriteStream('assets/js/ASSET_alloy-editor-all-min.js'))
+
+    //create alloy editor css file
+    fs.createReadStream('node_modules/alloyeditor/dist/alloy-editor/assets/alloy-editor-moono-min.css')
+        .pipe(fs.createWriteStream('assets/css/ASSET_allow-editor-moono-min.css'))
+}
